ce69bacfc12b3c23f43fe1d6250471adba27a9eb
msgreentea <msgreentea8@gmail.com>
2021-11-10 09:27:52 +0900

    modelから

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Fecades\DB;
+
+class ClientController extends Controller
+{
+    // public function add()
+    // {
+    //     return view('add');
+    // }
+    // public function create(Request $request)
+    // {
+    //     'name'
+    // }
+}
---
+++ app/Http/Controllers/PraController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Fecades\DB;
+
+class PraController extends Controller
+{
+    // public function index()
+    // {
+    //     $items = DB::select('select * from ')
+    // }
+}
